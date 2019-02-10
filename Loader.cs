3dfa36da7b3b8ffbdf69a2959890e45f9d5b18a7
ToxicTrigger <syoa1996@gmail.com>
2019-02-10 13:10:08 +0900

    :sparkles: New Add
    Ya!

---
+++ Loader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//author : ToxicTrigger
+//해당 컴포넌트는 붙어있는 프리팹의 정보들을 간단히 설정하기 위해 존재합니다. 
+[ExecuteInEditMode, RequireComponent(typeof(ScriptLoader), typeof(MeshRenderer), typeof(MeshFilter))]
+public class Loader : MonoBehaviour
+{
+    public bool ResetComponent;
+    public string ScriptFile;
+    public string MeshFile;
+
+    void Awake()
+    {
+        var script_loader = gameObject.GetComponent<ScriptLoader>();
+        if(script_loader != null) 
+        {
+            script_loader.reset = this.ResetComponent;
+            script_loader.ScriptName = this.ScriptFile;
+        }
+        else
+        {
+            Debug.LogWarning("Can't Find " + this.ScriptFile + " File ");
+        }
+
+        var mesh_filter = gameObject.GetComponent<MeshFilter>();
+        var mesh_rend = gameObject.GetComponent<MeshRenderer>();
+        var mesh = Resources.Load<MeshFilter>(MeshFile);
+        var rend = Resources.Load<MeshRenderer>(MeshFile);
+
+        // 메시 정보가 없을 경우의 예외처리
+        if( mesh_filter != null && mesh != null && rend != null)
+        {
+            mesh_filter.mesh = mesh.sharedMesh;
+            mesh_rend.materials = rend.sharedMaterials;
+        }
+        else
+        {
+            Debug.LogWarning("Can't Find " + MeshFile);
+        }
+    }
+}
---
+++ ScriptLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//author : ToxicTrigger
+//인자로 들어온 이름의 스크립트를 찾아 로드합니다.
+[ExecuteInEditMode]
+public class ScriptLoader : MonoBehaviour
+{
+    [Tooltip("스크립트를 찾아 로드합니다.")]
+    public string ScriptName;
+    public bool reset;
+    void Start() 
+    {
+        this.hideFlags = HideFlags.HideInInspector;
+
+        if(ScriptName.equals(""))
+        {
+            Debug.LogWarning(gameObject.name + " Can't Found " + ScriptName);
+        }
+        else
+        {
+            var type = Type.GetType(this.ScriptName);
+            // 해당 게임 오브젝트에 원하는 컴포넌트가 있다면 삭제합니다.
+            if (reset && gameObject.GetComponent(type) != null)
+            {
+                DestroyImmediate(gameObject.GetComponent(Type.GetType(this.ScriptName)));
+            }
+            Component component = null;
+            if(gameObject.GetComponent(type) == null)
+            {
+                component = gameObject.AddComponent(Type.GetType(this.ScriptName));
+            }
+            
+            if (component == null)
+            {
+                Debug.LogWarning(gameObject.name + " Can't Found " + ScriptName);
+            }
+            else
+            {
+                Debug.Log("Loaded " + ScriptName);
+            }
+        }
+    }
+    
+}
